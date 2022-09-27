eaab8938cfe786755151473a7427f03e347f0940
FadyEilia2001 <102275128+FadyEilia2001@users.noreply.github.com>
2022-09-26 21:47:54 -0500

    created login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Login = () => {
   return (
-    <div className="w-full border flex">
+    <div className="">
       <h2 className="">Login</h2>
     </div>
   );
